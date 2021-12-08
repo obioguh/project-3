2862a3942140c04ba8237326de10857cb4a537fd
obioguh <93559254+obioguh@users.noreply.github.com>
2021-12-08 16:31:56 -0500

    Update project3.s

--- project3.s
+++ project3.s
@@ -211,6 +211,15 @@ SubProgramC:
     add $s7, $s1, $s7
     sgt $t8, $a1, 64
     slt $a3, $a1, $s7
+    and $t8, $t8, $a3
+    bne $t8, $zero, upperLetter
+    
+    li $s7, 97
+    add $s7, $s7, $s1
+    sgt $t8, $a1, 96
+    slt $a3, $a1, $s7
+    and $t8, $t8, $a3
+    bne $t8, $zero, lowerLetter
         
     
     
